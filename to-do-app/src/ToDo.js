5b55c38b545b58bebbc0b02a2f1785e4b2cd21f0
hokkichan <57009866+hokkichan@users.noreply.github.com>
2020-09-20 15:20:14 +0800

    Update add-and-delete function

--- to-do-app/src/ToDo.js
+++ to-do-app/src/ToDo.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+
+
 function TodosComponent() {
   const [currentTodo, setCurrentTodo] = useState("");
   const [todos, setTodos] = useState([
@@ -16,14 +18,65 @@ function TodosComponent() {
       isCompleted: false
     }
   ]);
-  return (
-    <div>
-      {todos.map((todo, index) => (
-        <p>{todo.todo}</p>
-      ))}
-      {todos.length > 0 && `${todos.length} items`}
-    </div>
-  );
+
+  function createNewTodo(currentTodo) {
+  let todosArray = [...todos];
+  todosArray.push({
+    todo: currentTodo,
+    isCompleted: false
+  });
+  setTodos(todosArray);
 }
+
+function completeTodo(index) {
+  const todosArray = [...todos];
+  todosArray[index].isCompleted = !todosArray[index].isCompleted;
+  setTodos(todosArray);
+}
+
+function deleteTodo(index) {
+  let todosArray = [...todos];
+  todosArray.splice(index, 1);
+  setTodos(todosArray);
+}
+
+  return (
+  <div>
+    <input
+      className="todo-input"
+      value={currentTodo}
+      onChange={e => {
+        setCurrentTodo(e.target.value);
+      }}
+      onKeyPress={e => {
+        if (e.key === "Enter") {
+          createNewTodo(currentTodo);
+          setCurrentTodo("");
+        }
+      }}
+      placeholder="What needs to get done?"
+    />
+    {
+  todos.map((todo, index) => (
+    <div key={todo} className="todo">
+      <div className="checkbox" onClick={() => completeTodo(index)}>
+        {todo.isCompleted && <span>&#x2714;</span>}
+      </div>
+      <div className={todo.isCompleted ? "done" : ""}>{todo.todo}</div>
+      <div className="delete" onClick={() => deleteTodo(index)}>
+  &#128465;
+</div>
+    </div>
+  ))
+}
+    {todos.map((todo, index) => (
+      <p></p>
+    ))}
+    {todos.length > 0 && `${todos.length} items`}
+  </div>
+);
+}
+
+
 
 export default TodosComponent;
